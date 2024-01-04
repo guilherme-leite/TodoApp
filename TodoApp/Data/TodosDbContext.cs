b40dcd7a9c22066432e182c6afae7fa670900a56
Guilherme Leite <guilhermelax@hotmail.com>
2024-01-04 14:42:36 -0300

    Feat: Creating the DbContext.

---
+++ TodoApp/Data/TodosDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp.Models;
+
+namespace TodoApp.Data
+{
+    public class TodosDbContext : DbContext
+    {
+        protected TodosDbContext(DbContextOptions<TodosDbContext> options)
+        : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
