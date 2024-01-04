2393bb6ff6c463b5aa5639ea4f73a19105bf442a
Guilherme Leite <guilhermelax@hotmail.com>
2024-01-04 13:33:50 -0300

    Feat: Creating model layer and entities.

---
+++ TodoApp/Models/Todo.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApp.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public int Status { get; set; }
+
+    }
+}
