128564a5557dd8c7b6a85843597c492e4255dfa2
Guilherme Leite <guilhermelax@hotmail.com>
2024-01-04 16:17:51 -0300

    Feat: Mapping todo and user atributes.

---
+++ TodoApp/Data/Map/TodoMap.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TodoApp.Models;
+
+namespace TodoApp.Data.Map
+{
+    public class TodoMap : IEntityTypeConfiguration<Todo>
+    {
+        public void Configure(EntityTypeBuilder<Todo> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Title).IsRequired().HasMaxLength(250);
+            builder.Property(x => x.Description).HasMaxLength(1000);
+            builder.Property(x => x.Status).IsRequired();
+        }
+    }
+}
---
+++ TodoApp/Data/Map/UserMap.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TodoApp.Models;
+
+namespace TodoApp.Data.Map
+{
+    public class UserMap : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(250);
+            builder.Property(x => x.Email).IsRequired().HasMaxLength(150);
+        }
+    }
+}
