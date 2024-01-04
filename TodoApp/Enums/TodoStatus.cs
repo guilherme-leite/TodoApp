cc405c962c01f260a8169081c17432612a28e449
Guilherme Leite <guilhermelax@hotmail.com>
2024-01-04 16:09:36 -0300

    Feat: Creating todo status enum.

---
+++ TodoApp/Enums/TodoStatus.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace TodoApp.Enums
+{
+    public enum TodoStatus
+    {
+        [Description("Todo")]
+        Todo = 1,
+        [Description("In progress")]
+        InProgress = 2,
+        [Description("Completed")]
+        Completed = 3
+    }
+}
