093b225cb7ac2fc76322c720b346d6f65a970e9e
Guilherme Leite <guilhermelax@hotmail.com>
2024-01-04 13:37:21 -0300

    Feat: Creating basic UserController.

---
+++ TodoApp/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TodoApp.Models;
+
+namespace TodoApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<User>> GetAllUsers()
+        {
+            return Ok();
+        }
+    }
+}
