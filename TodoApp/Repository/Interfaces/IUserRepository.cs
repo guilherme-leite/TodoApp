c5b07812d51a072900cb38280fb1ccbf05bbf8db
Guilherme Leite <guilhermelax@hotmail.com>
2024-01-04 14:43:25 -0300

    Feat: Creating User interface.

---
+++ TodoApp/Repository/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using TodoApp.Models;
+
+namespace TodoApp.Repository.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<List<User>> FindAll();
+        Task<User> FindById(int id);
+        Task<User> Add(User user);
+        Task<User> Update(User user, int id);
+        Task<bool> Delete(int id);
+    }
+}
